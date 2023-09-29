4e4121e03e59a0df72c9d45f01db30b7866f6d11
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-29 17:36:24 +0800

    added layouts and classes

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer className="h-14 p-4 bg-slate-950">Footer</footer>;
+};
+
+export default Footer;
---
+++ app/new/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const page = () => {
+  return <div className="content-height ">New</div>;
+};
+
+export default page;
+ 
