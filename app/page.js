91098bb095461751d25a434e5c4082209f1ccf77
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-30 18:49:56 +0800

    added login to index

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Link from "next/link";
-
+"use client";
+import { signIn } from "next-auth/react";
 export default function Home() {
   return (
     <>
@@ -8,9 +8,12 @@ export default function Home() {
           <h1 className="text-2xl text-center text-slate-200">Main Content</h1>
           <div>
             <h2 className="text-xl mb-4">Wazzup</h2>
-            <Link href={"/login"} className="bg-white text-black p-2">
+            <button
+              onClick={() => signIn()}
+              className="bg-white text-black p-2"
+            >
               LOGIN
-            </Link>
+            </button>
           </div>
         </div>
       </div>
