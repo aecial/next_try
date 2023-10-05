a333a5578e3bd460d02f89eb1d13068ec0a410e3
Teddy Pascual <teddylayson2000@gmail.com>
2023-10-05 10:23:45 +0800

    added createdAt column

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 // "use client";
 import { PrismaClient } from "@prisma/client";
+
 export default async function Home() {
   const prisma = new PrismaClient();
   const notes = await prisma.notes.findMany();
+
   return (
     <>
       <div className="content-height p-4">
@@ -10,15 +12,17 @@ export default async function Home() {
           <h1 className="text-2xl text-center text-slate-200">Main Content</h1>
           <div>
             <h2 className="text-xl mb-4">Wazzup</h2>
-            <table className=" mx-auto border border-white  w-44 text-center">
+            <table className=" mx-auto border border-white  w-auto text-center">
               <th>Id</th>
               <th>Text</th>
+              <th>CreatedAt</th>
               <tbody className="">
                 {notes.map((note) => {
                   return (
                     <tr>
                       <td>{note.id}</td>
                       <td>{note.text}</td>
+                      <td>{note.createdAt.toString()}</td>
                     </tr>
                   );
                 })}
