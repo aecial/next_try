fc965be1f0771ce4304baf9983d470a86b3b8dee
Teddy Pascual <teddylayson2000@gmail.com>
2023-10-11 19:46:21 +0800

    added base input for search note

--- app/searchNote/page.js
+++ app/searchNote/page.js
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+import { toast } from "sonner";
+import React, { useState } from "react";
+import NoteCard from "@/components/NoteCard";
 
 const page = () => {
-  return <div>searchNote</div>;
+  const [noteId, setNoteId] = useState("");
+
+  // search note to DB
+  async function searchNote(noteId) {
+    console.log(noteId);
+    setNoteId("");
+  }
+
+  return (
+    <div className="content-height flex items-center justify-center">
+      <div className="flex flex-col">
+        <input
+          type="text"
+          className="text-2xl mb-4 text-slate-800 px-0.5"
+          value={noteId}
+          onChange={(e) => setNoteId(e.target.value)}
+          required
+        />
+        <button
+          onClick={() => searchNote(noteId)}
+          className="text-md border border-white"
+        >
+          Search Note
+        </button>
+      </div>
+      {/* <NoteCard /> */}
+    </div>
+  );
 };
 
 export default page;
