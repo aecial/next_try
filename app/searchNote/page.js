5353e34920244da01674556eab4eaf17b1a3da08
Teddy Pascual <teddylayson2000@gmail.com>
2023-10-13 07:59:32 +0800

    implemented search note using API

--- app/searchNote/page.js
+++ app/searchNote/page.js
@@ -2,18 +2,28 @@
 import { toast } from "sonner";
 import React, { useState } from "react";
 import NoteCard from "@/components/NoteCard";
+import { stringify } from "postcss";
 
 const page = () => {
   const [noteId, setNoteId] = useState("");
+  const [validId, setValidId] = useState("");
+  const [validText, setValidText] = useState("");
 
   // search note to DB
   async function searchNote(noteId) {
     console.log(noteId);
+    const noteData = await fetch(`api/note/${noteId}`);
+    const info = await noteData.json();
+    setValidId(info["data"]["id"]);
+    setValidText(info["data"]["text"]);
     setNoteId("");
+    // setValidId(data.data.id);
+    // setValidText(data.data.text);
+    // setValidDate(data.data.createdAt);
   }
 
   return (
-    <div className="content-height flex items-center justify-center">
+    <div className="content-height flex  items-center justify-center">
       <div className="flex flex-col">
         <input
           type="text"
@@ -29,7 +39,7 @@ const page = () => {
           Search Note
         </button>
       </div>
-      {/* <NoteCard /> */}
+      <NoteCard id={validId} text={validText} date={""} />
     </div>
   );
 };
